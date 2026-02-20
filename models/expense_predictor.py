03b7b092bfe81d8cad67e8d9368ebf1a292693e2
Jashwanth <jashwanth.chowdary08@gmail.com>
2026-02-20 16:06:34 +0530

    fixed expense predictor argument issue

--- models/expense_predictor.py
+++ models/expense_predictor.py
@@ -1,35 +1,18 @@
-import mysql.connector
-import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def predict_expense():
+def predict_expense(amounts):
 
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Jashwanth@18",
-        database="financial_analysis"
-    )
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT date, amount FROM expenses")
-    data = cursor.fetchall()
-
-    if len(data) < 2:
+    if len(amounts) == 0:
         return 0
 
-    df = pd.DataFrame(data, columns=["date", "amount"])
-    df["date"] = pd.to_datetime(df["date"])
-    df["day"] = df["date"].dt.day
-
-    X = df[["day"]]
-    y = df["amount"]
+    X = np.array(range(len(amounts))).reshape(-1, 1)
+    y = np.array(amounts)
 
     model = LinearRegression()
     model.fit(X, y)
 
-    next_day = [[30]]
-    prediction = model.predict(next_day)
+    next_month = np.array([[len(amounts)]])
+    prediction = model.predict(next_month)
 
     return round(prediction[0], 2)
