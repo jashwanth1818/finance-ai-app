9d823d3545e50144c237d8169f2161682c75dd92
Jashwanth <jashwanth.chowdary08@gmail.com>
2026-02-20 17:30:28 +0530

    fixed localhost db in risk detector

--- models/risk_detector.py
+++ models/risk_detector.py
@@ -1,13 +1,49 @@
+import os
+import mysql.connector
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from urllib.parse import urlparse
 
 
-def detect_risk(amounts):
+def get_connection():
 
-    if not amounts:
-        return "No Data"
+    db_url = os.getenv("MYSQL_URL")
 
-    avg = sum(amounts) / len(amounts)
+    url = urlparse(db_url)
 
-    if avg > 5000:
-        return "High Risk Spending ⚠️"
+    return mysql.connector.connect(
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path.replace('/', ''),
+        port=url.port
+    )
+
+
+def detect_risk():
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT amount FROM expenses")
+    data = cursor.fetchall()
+
+    if len(data) < 5:
+        return "Low Risk"
+
+    df = pd.DataFrame(data, columns=["amount"])
+
+    df["risk"] = df["amount"].apply(lambda x: 1 if x > 5000 else 0)
+
+    X = df[["amount"]]
+    y = df["risk"]
+
+    model = DecisionTreeClassifier()
+    model.fit(X, y)
+
+    prediction = model.predict([[6000]])
+
+    if prediction[0] == 1:
+        return "High Risk"
     else:
-        return "Low Risk Spending ✅"
+        return "Low Risk"
